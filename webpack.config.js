1d6d64f2908efffab4026c1edc372bfa003b52e1
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-05-14 20:41:42 -0300

    feat: project base structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { Configuration } = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+/**
+ * @type {Configuration}
+ */
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+  devServer: {
+    port: 8000,
+  },
+};
